dd420b6dd0684ada4e519ae2a4d8fcdbc9f6b654
Django Summerill <98916540+djangosummerill@users.noreply.github.com>
2025-06-11 21:06:28 +0100

    move components to pages

--- app/auth/forgotpassword/page.tsx
+++ app/auth/forgotpassword/page.tsx
@@ -1,7 +1,52 @@
-import { ForgotPassword } from "@/components/forgot-password-form";
-import { SignInForm } from "@/components/signin-form";
-import { IconPrism } from "@tabler/icons-react";
-
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 export default function SignInPage() {
-  return <ForgotPassword />;
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Forgot password?</CardTitle>
+          <CardDescription>Get sent a password reset link</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="theo@t3.gg"
+                    required
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Submit
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Remembered your password?{" "}
+                <a href="signin" className="underline underline-offset-4">
+                  Sign in
+                </a>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  );
 }
