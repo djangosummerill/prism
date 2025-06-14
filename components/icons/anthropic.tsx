ea60b37ce6db9372549593e1fd7e9770e3265ce5
Django Summerill <98916540+djangosummerill@users.noreply.github.com>
2025-06-14 22:30:53 +0100

    fix icons

--- components/icons/anthropic.tsx
+++ components/icons/anthropic.tsx
@@ -4,10 +4,15 @@ export function Anthropic(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={16}
-      height={16}
+      width="1em"
+      height="1em"
       fill="currentColor"
-      className="bi bi-anthropic"
+      fillRule="evenodd"
+      style={{
+        flex: "none",
+        lineHeight: 1,
+      }}
+      viewBox="0 0 16 16"
       {...props}
     >
       <path
